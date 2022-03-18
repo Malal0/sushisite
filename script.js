4b3e06c9a781c0b295034d300f833a43901e40a2
UncomfortableChai <mj1055119@gmail.com>
2022-03-18 13:48:00 -0700

    fixed the join our team page and some css

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//      Join Our Family     //
+const jof_Btn = document.getElementById("submitBtn");
+jof_Btn.addEventListener("click", () => {
+    console.log("submitted");
+    alert("submitted");
+});
+
