5cbd141693d64aa15db31c62e09df6a2a6694143
UncomfortableChai <mj1055119@gmail.com>
2022-04-03 18:21:42 -0700

    fixed issues

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 //      Join Our Family     //
 const jof_Btn = document.getElementById("submitBtn");
+
 jof_Btn.addEventListener("click", () => {
     console.log("submitted");
     alert("submitted");
