59bf6521216a2ceb6328546d83bde1090d4c44ea
UncomfortableChai <mj1055119@gmail.com>
2022-05-15 01:02:51 -0700

    fixing shop scripts

--- script.js
+++
@@ -1,9 +0,0 @@
-
-//      Join Our Family     //
-const jof_Btn = document.getElementById("submitBtn");
-
-jof_Btn.addEventListener("click", () => {
-    console.log("submitted");
-    alert("submitted");
-});
-
